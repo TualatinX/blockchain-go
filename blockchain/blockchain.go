6a3e4c714907d360cd7f26855014ad4d959cae15
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-05-01 23:49:43 +0800

    feat: FindUnspentTransactions, Find unspent outputs

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,6 +1,7 @@
 package blockchain
 
 import (
+	"encoding/hex"
 	"fmt"
 	"os"
 	"runtime"
@@ -98,6 +99,64 @@ func ContinueBlockChain(address string) *BlockChain {
 
 	chain := BlockChain{lastHash, db}
 	return &chain
+}
+
+func (chain *BlockChain) FindUnspentTransactions(address string) []Transaction {
+	var unspentTxs []Transaction
+
+	spentTXOs := make(map[string][]int)
+
+	iter := chain.Iterator()
+
+	for {
+		block := iter.Next()
+
+		for _, tx := range block.Transactions {
+			txID := hex.EncodeToString(tx.ID)
+
+		Outputs:
+			for outIdx, out := range tx.Outputs {
+				if spentTXOs[txID] != nil {
+					for _, spentOut := range spentTXOs[txID] {
+						if spentOut == outIdx {
+							continue Outputs
+						}
+					}
+				}
+				if out.CanBeUnlocked(address) {
+					unspentTxs = append(unspentTxs, *tx)
+				}
+			}
+			if !tx.IsCoinbase() {
+				for _, in := range tx.Inputs {
+					if in.CanUnlock(address) {
+						inTxID := hex.EncodeToString(in.ID)
+						spentTXOs[inTxID] = append(spentTXOs[inTxID], in.Out)
+					}
+				}
+			}
+		}
+
+		if len(block.PrevHash) == 0 {
+			break
+		}
+	}
+	return unspentTxs
+}
+
+// Find unspent outputs
+func (chain *BlockChain) FindUTXO(address string) []TxOutput {
+	var UTXOs []TxOutput
+	unspentTransactions := chain.FindUnspentTransactions(address)
+	for _, tx := range unspentTransactions {
+		for _, out := range tx.Outputs {
+			if out.CanBeUnlocked(address) {
+				UTXOs = append(UTXOs, out)
+			}
+		}
+	}
+
+	return UTXOs
 }
 
 func (chain *BlockChain) AddBlock(data string) {
--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -16,24 +16,26 @@ type Transaction struct {
 }
 
 type TxOutput struct {
-	Value int
 	// Value would be representative of the amount of coins in a transaction
-	PubKey string
+	Value int
+
 	// The Pubkey is needed to "unlock" any coins within an Output. This indicated that YOU are the one that sent it.
 	// You are indentifiable by your PubKey
 	// PubKey in this iteration will be very straightforward, however in an actual application this is a more complex algorithm
+	PubKey string
 }
 
 //TxInput is representative of a reference to a previous TxOutput
 type TxInput struct {
-	ID []byte
 	// ID will find the Transaction that a specific output is inside of
-	Out int
+	ID []byte
+
 	// Out will be the index of the specific output we found within a transaction.
 	// For example if a transaction has 4 outputs, we can use this "Out" field to specify which output we are looking for
+	Out int
+
+	// This would be a script that adds data to an outputs' PubKey. However for this tutorial the Sig will be indentical to the PubKey.
 	Sig string
-	// This would be a script that adds data to an outputs' PubKey
-	// however for this tutorial the Sig will be indentical to the PubKey.
 }
 
 // CoinbaseTx is the function that will run when someone on a node succesfully "mines" a block. The reward inside as it were.
