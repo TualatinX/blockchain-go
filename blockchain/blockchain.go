9c78c87a73af10fe20d472226b3d3286d78cd778
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-05-02 10:09:52 +0800

    feat: FindSpendableOutputs

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -159,6 +159,28 @@ func (chain *BlockChain) FindUTXO(address string) []TxOutput {
 	return UTXOs
 }
 
+func (chain *BlockChain) FindSpendableOutputs(address string, amount int) (int, map[string][]int) {
+	unspentOuts := make(map[string][]int)
+	unspentTxs := chain.FindUnspentTransactions(address)
+	accumulated := 0
+
+Work:
+	for _, tx := range unspentTxs {
+		txID := hex.EncodeToString(tx.ID)
+		for outIdx, out := range tx.Outputs {
+			if out.CanBeUnlocked(address) && accumulated < amount {
+				accumulated += out.Value
+				unspentOuts[txID] = append(unspentOuts[txID], outIdx)
+
+				if accumulated >= amount {
+					break Work
+				}
+			}
+		}
+	}
+	return accumulated, unspentOuts
+}
+
 func (chain *BlockChain) AddBlock(data string) {
 	var lastHash []byte
 
