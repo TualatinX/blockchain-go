a22e35803ed3f68ac49c7ceab835072a8b29c84b
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-05-02 12:29:01 +0800

    feat: singular wallet part

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -4,7 +4,10 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"crypto/sha256"
 	"log"
+
+	"golang.org/x/crypto/ripemd160"
 )
 
 const (
@@ -32,4 +35,38 @@ func NewKeyPair() (ecdsa.PrivateKey, []byte) {
 	pub := append(private.PublicKey.X.Bytes(), private.PublicKey.Y.Bytes()...)
 
 	return *private, pub
+}
+
+func PublicKeyHash(publicKey []byte) []byte {
+	hashedPublicKey := sha256.Sum256(publicKey)
+
+	hasher := ripemd160.New()
+	_, err := hasher.Write(hashedPublicKey[:])
+	if err != nil {
+		log.Panic(err)
+	}
+	publicRipeMd := hasher.Sum(nil)
+
+	return publicRipeMd
+}
+
+func Checksum(ripeMdHash []byte) []byte {
+	firstHash := sha256.Sum256(ripeMdHash)
+	secondHash := sha256.Sum256(firstHash[:])
+
+	return secondHash[:checksumLength]
+}
+
+func (w *Wallet) Address() []byte {
+	// Step 1/2
+	pubHash := PublicKeyHash(w.PublicKey)
+	//Step 3
+	versionedHash := append([]byte{version}, pubHash...)
+	//Step 4
+	checksum := Checksum(versionedHash)
+	//Step 5
+	finalHash := append(versionedHash, checksum...)
+	//Step 6
+	address := base58Encode(finalHash)
+	return address
 }
