997f293de08479e285234a92a322be2c5f92bb52
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-05-02 12:12:25 +0800

    fix: util -> wallet

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,1 +1,35 @@
 package wallet
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"log"
+)
+
+const (
+	checksumLength = 4
+
+	//hexadecimal representation of 0
+	version = byte(0x00)
+)
+
+type Wallet struct {
+	//ecdsa = elliptic curve digital signature algorithm
+	PrivateKey ecdsa.PrivateKey
+
+	PublicKey []byte
+}
+
+func NewKeyPair() (ecdsa.PrivateKey, []byte) {
+	curve := elliptic.P256()
+
+	private, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	pub := append(private.PublicKey.X.Bytes(), private.PublicKey.Y.Bytes()...)
+
+	return *private, pub
+}
