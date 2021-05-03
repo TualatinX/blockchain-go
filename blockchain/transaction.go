709189781a500cf8f95e4e5a0cea4691d3ae285b
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-05-03 23:39:27 +0800

    feat(transaction): sign & verify

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -2,11 +2,16 @@ package blockchain
 
 import (
 	"bytes"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
 	"crypto/sha256"
 	"encoding/gob"
 	"encoding/hex"
 	"fmt"
 	"log"
+	"math/big"
+	"strings"
 )
 
 const reward = 100
@@ -15,6 +20,50 @@ type Transaction struct {
 	ID      []byte
 	Inputs  []TxInput
 	Outputs []TxOutput
+}
+
+func (tx Transaction) String() string {
+	lines := []string{}
+
+	lines = append(lines, fmt.Sprintf("—— Transaction %x:", tx.ID))
+	for inputId, input := range tx.Inputs {
+		lines = append(lines, fmt.Sprintf("\tInput %d", inputId))
+		lines = append(lines, fmt.Sprintf("\t\tTXID: %x", input.ID))
+		lines = append(lines, fmt.Sprintf("\t\tOut: %d", input.Out))
+		lines = append(lines, fmt.Sprintf("\t\tSignature: %x", input.Signature))
+		lines = append(lines, fmt.Sprintf("\t\tPubKey: %x", input.PubKey))
+	}
+
+	for outputId, output := range tx.Outputs {
+		lines = append(lines, fmt.Sprintf("\tOutput %d", outputId))
+		lines = append(lines, fmt.Sprintf("\t\tValue: %d", output.Value))
+		lines = append(lines, fmt.Sprintf("\t\tPubKeyHash: %x", output.PubKeyHash))
+	}
+
+	return strings.Join(lines, "\n")
+
+}
+
+func (tx Transaction) Serialize() []byte {
+	var encoded bytes.Buffer
+
+	enc := gob.NewEncoder(&encoded)
+	err := enc.Encode(tx)
+	if err != nil {
+		log.Panic(err)
+	}
+	return encoded.Bytes()
+}
+
+func (tx *Transaction) Hash() []byte {
+	var hash [32]byte
+
+	txCopy := *tx
+	txCopy.ID = []byte{}
+
+	hash = sha256.Sum256(txCopy.Serialize())
+
+	return hash[:]
 }
 
 // CoinbaseTx is the function that will run when someone on a node succesfully "mines" a block. The reward inside as it were.
@@ -86,4 +135,92 @@ func NewTransaction(from, to string, amount int, chain *BlockChain) *Transaction
 	tx.SetID()
 
 	return &tx
+}
+
+func (tx *Transaction) TrimmedCopy() Transaction {
+	var inputs []TxInput
+	var outputs []TxOutput
+
+	for _, in := range tx.Inputs {
+		inputs = append(inputs, TxInput{in.ID, in.Out, nil, nil})
+	}
+
+	outputs = append(outputs, tx.Outputs...)
+
+	txCopy := Transaction{[]byte{}, inputs, outputs}
+	return txCopy
+
+}
+
+func (tx *Transaction) Sign(privateKey ecdsa.PrivateKey, previousTXs map[string]Transaction) {
+	if tx.IsCoinbase() {
+		return
+	}
+
+	for _, in := range previousTXs {
+		if previousTXs[hex.EncodeToString(in.ID)].ID == nil {
+			log.Panic("Error: Previous transaction does not exist")
+		}
+	}
+
+	txCopy := tx.TrimmedCopy()
+
+	for inId, in := range txCopy.Inputs {
+		prevTx := previousTXs[hex.EncodeToString(in.ID)]
+		txCopy.Inputs[inId].Signature = nil
+		txCopy.Inputs[inId].PubKey = prevTx.Outputs[in.Out].PubKeyHash
+		txCopy.ID = txCopy.Hash()
+		txCopy.Inputs[inId].PubKey = nil
+
+		r, s, err := ecdsa.Sign(rand.Reader, &privateKey, txCopy.ID)
+		Handle(err)
+		signature := append(r.Bytes(), s.Bytes()...)
+
+		tx.Inputs[inId].Signature = signature
+	}
+}
+
+func (tx *Transaction) Verify(prevTxs map[string]Transaction) bool {
+	if tx.IsCoinbase() {
+		return true
+	}
+
+	for _, in := range tx.Inputs {
+		if prevTxs[hex.EncodeToString(in.ID)].ID == nil {
+			log.Panic("Error: Previous transaction does not exist")
+		}
+	}
+
+	txCopy := tx.TrimmedCopy()
+	curve := elliptic.P256()
+
+	for inId, in := range txCopy.Inputs {
+		prevTx := prevTxs[hex.EncodeToString(in.ID)]
+		txCopy.Inputs[inId].Signature = nil
+		txCopy.Inputs[inId].PubKey = prevTx.Outputs[in.Out].PubKeyHash
+		txCopy.ID = txCopy.Hash()
+		txCopy.Inputs[inId].PubKey = nil
+
+		r := big.Int{}
+		s := big.Int{}
+		// first half of the signature is r and the second half is s
+		sigLen := len(in.Signature)
+		r.SetBytes(in.Signature[:(sigLen / 2)])
+		s.SetBytes(in.Signature[(sigLen / 2):])
+
+		x := big.Int{}
+		y := big.Int{}
+
+		pubLen := len(in.PubKey)
+		x.SetBytes(in.PubKey[:(pubLen / 2)])
+		y.SetBytes(in.PubKey[(pubLen / 2):])
+
+		rawPublicKey := ecdsa.PublicKey{Curve: curve, X: &x, Y: &y}
+
+		if !ecdsa.Verify(&rawPublicKey, txCopy.ID, &r, &s) {
+			return false
+		}
+	}
+
+	return true
 }
