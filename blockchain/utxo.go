18d8263453a063a6176ff53122a9d207e9fb9dae
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-05-08 22:36:49 +0800

    feat(utxo): DeleteByPrefix

--- blockchain/utxo.go
+++ blockchain/utxo.go
@@ -1,5 +1,11 @@
 package blockchain
 
+import (
+	"log"
+
+	"github.com/dgraph-io/badger"
+)
+
 var (
 	utxoPrefix   = []byte("utxo-")
 	prefixLength = len(utxoPrefix)
@@ -7,5 +13,50 @@ var (
 
 // Unspent transaction outputs
 type UTXOSet struct {
-	BlockChain *BlockChain
+	Blockchain *BlockChain
+}
+
+func (u *UTXOSet) DeleteByPrefix(prefix []byte) {
+	deleteKeys := func(keysForDelete [][]byte) error {
+		if err := u.Blockchain.Database.Update(func(txn *badger.Txn) error {
+			for _, key := range keysForDelete {
+				if err := txn.Delete(key); err != nil {
+					return err
+				}
+			}
+			return nil
+		}); err != nil {
+			return err
+		}
+		return nil
+	}
+
+	collectSize := 100000
+	u.Blockchain.Database.View(func(txn *badger.Txn) error {
+		opts := badger.DefaultIteratorOptions
+		opts.PrefetchValues = false
+		it := txn.NewIterator(opts)
+		defer it.Close()
+
+		keysForDelete := make([][]byte, 0, collectSize)
+		keysCollected := 0
+		for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+			key := it.Item().KeyCopy(nil)
+			keysForDelete = append(keysForDelete, key)
+			keysCollected++
+			if keysCollected == collectSize {
+				if err := deleteKeys(keysForDelete); err != nil {
+					log.Panic(err)
+				}
+				keysForDelete = make([][]byte, 0, collectSize)
+				keysCollected = 0
+			}
+		}
+		if keysCollected > 0 {
+			if err := deleteKeys(keysForDelete); err != nil {
+				log.Panic(err)
+			}
+		}
+		return nil
+	})
 }
