32c4506a9991229b15c0bcd55013547587f14e67
Yinghao Zhu <zhuyinghao1999@msn.cn>
2021-05-09 13:20:26 +0800

    chore: introduce death

--- network/network.go
+++ network/network.go
@@ -2,6 +2,12 @@ package network
 
 import (
 	"blockchain-go/blockchain"
+	"fmt"
+	"os"
+	"runtime"
+	"syscall"
+
+	"github.com/vrecan/death/v3"
 )
 
 const (
@@ -53,4 +59,36 @@ type Version struct {
 	Version    int
 	BestHeight int // length of actual chain
 	AddrFrom   string
+}
+
+func CmdToBytes(cmd string) []byte {
+	var bytes [commandLength]byte
+
+	for i, c := range cmd {
+		bytes[i] = byte(c)
+	}
+
+	return bytes[:]
+}
+
+func BytesToCmd(bytes []byte) string {
+	var cmd []byte
+
+	for _, b := range bytes {
+		if b != 0x0 {
+			cmd = append(cmd, b)
+		}
+	}
+
+	return fmt.Sprintf("%s", cmd)
+}
+
+func CloseDB(chain *blockchain.BlockChain) {
+	d := death.NewDeath(syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+
+	d.WaitForDeathWithFunc(func() {
+		defer os.Exit(1)
+		defer runtime.Goexit()
+		chain.Database.Close()
+	})
 }
